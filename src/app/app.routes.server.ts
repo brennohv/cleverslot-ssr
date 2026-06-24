0d3f2f9ddf22af9fba4f9e2da63ca492ecabb059
brennohv <brennovicentini@gmail.com>
2026-06-24 14:05:21 +0100

    revert server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,31 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'auth/**',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'landing-page',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: ':barbershopSlug/admin/**',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: ':barbershopSlug/barber/**',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: '',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: ':barbershopSlug',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
 ];
